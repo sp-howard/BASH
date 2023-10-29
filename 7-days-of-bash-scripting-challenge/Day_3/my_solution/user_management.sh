fd68f5a0a9ed59055e2ef8240b2bb8d79fb91bf5
sp-howard <stevenphoward@gmail.com>
2023-10-29 16:51:13 -0700

    user_management script near completion

--- 7-days-of-bash-scripting-challenge/Day_3/my_solution/user_management.sh
+++ 7-days-of-bash-scripting-challenge/Day_3/my_solution/user_management.sh
@@ -1,6 +1,100 @@
 #!/bin/bash
 
-## GETOPT
+#############
+# FUNCTIONS #
+#############
+
+## Account Creation
+## -c or --create
+
+account_creation() {
+
+    #Prompt for username if name is not passed to function
+    if [ $# -eq 1 ]; then
+        username=$1
+    else
+        read -p "Enter new user name. \n" username
+    fi
+
+    # Check if username exists in password file
+    # -q sets grep to quiet mode, so that only the exit code is retreived
+    if grep -q "$username" /etc/passwd; then
+        echo "There is already a user named $username."
+        exit 1
+    fi
+
+    useradd $ac_username
+    # Check if useradd command completes
+    if [ $? -ne 0 ]; then
+        echo "Failed to add user. Try running with super user privileges."
+        exit 1
+    else
+        passwd $username
+        echo "New user created: $username"
+    fi
+}
+
+### Account Deletion
+### -d or --delete
+
+account_deletion() {
+
+    #Prompt for username if name is not passed to function
+    if [ $# -eq 1 ]; then
+        username=$1
+    else
+        read -p "Enter user name to delete.\n" username
+    fi
+
+    userdel $username
+    # Check if useradd command completes
+    if [ $? -ne 0 ]; then
+        echo "Failed to delete user."
+        exit 1
+    else
+        echo "User $username was deleted."
+    fi
+}
+
+
+### Password Reset
+### -r or --reset
+
+password_reset() {
+
+    #Prompt for username if name is not passed to function
+    if [ $# -eq 1 ]; then
+        username=$1
+    else
+        read -p "Enter user name to reset password for.\n" username
+    fi
+
+    passwd $username
+    # Check if useradd command completes
+    if [ $? -ne 0 ]; then
+        echo "Failed to reset password for $username."
+        exit 1
+    else
+        echo "Password for $username was reset."
+    fi
+}
+
+### List Accounts
+### -l or --list
+
+list_accounts() {
+
+awk -F: -v min=$(awk '/^UID_MIN/ {print $2}' /etc/login.defs) -v max=$(awk '/^UID_MAX/ {print $2}' /etc/login.defs) '$3 >= min && $3 <= max {print $1}' /etc/passwd
+
+}
+
+### Display Help
+### -h or --help
+
+
+############
+#  GETOPT  #
+############
 
 # Validate options
 options=$(getopt -o cdrlh --long create:,delete:,reset:,list,help  -- "$@")
@@ -14,22 +108,47 @@ options=$(getopt -o cdrlh --long create:,delete:,reset:,list,help  -- "$@")
 eval set -- "$options"
 while true; do
     case "$1" in
-    -b)
-        COLOR=BLUE
+    -c)
+        account_creation
+        ;;
+    -d)
+        account_deletion
         ;;
     -r)
-        COLOR=RED
+        password_reset
         ;;
-    -g)
-        COLOR=GREEN
+    -l)
+        list_accounts
         ;;
-    --color)
+    -h)
+        echo "Help"
+        ;;
+    --create)
         shift; # The arg is next in position args
-        COLOR=$1
-        [[ ! $COLOR =~ BLUE|RED|GREEN ]] && {
-            echo "Incorrect options provided"
+        account_creation $1
+        [[ ! $1 ]] && {
+            echo "Please provide name of user to create."
             exit 1
         }
+        ;;
+    --delete)
+        shift; # The arg is next in position args
+        account_deletion $1
+        [[ ! $1 ]] && {
+            echo "Please provide name of user to delete."
+            exit 1
+        }
+        ;;
+    --reset)
+        shift; # The arg is next in position args
+        password_reset $1
+        [[ ! $1 ]] && {
+            echo "Please provide name of user whose password you'd like to reset."
+            exit 1
+        }
+        ;;
+    --list)
+        list_accounts
         ;;
     --)
         shift
@@ -39,41 +158,4 @@ while true; do
     shift
 done
 
-echo "Color is $COLOR"
 exit 0;
-
-
-### Account Creation
-### -c or --create
-
-echo "New username:"
-read ac_username
-
-# Check if username exists in password file
-# -q sets grep to quiet mode, so that only the exit code is retreived
-if grep -q "$ac_username" /etc/passwd
-then
-    echo "There is already a user named $ac_username"
-    exit 1
-fi
-
-useradd $ac_username
-passwd $ac_username
-
-echo "New user created: $ac_username"
-
-### Account Deletion
-### -d or --delete
-
-
-### Password Reset
-### -r or --reset
-
-
-### List Accounts
-### -l or --list
-
-
-### Display Help
-### -h or --help
-
