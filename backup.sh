ab2237d61403b5a5286632493a2999c744097408
sp-howard <stevenphoward@gmail.com>
2023-10-31 18:46:13 +0000

    Cleanup comments and whitespace.

--- backup.sh
+++ backup.sh
@@ -2,7 +2,6 @@
 
 # Backup Directory w/ Rotation (Retain Last 3 Backups)
 
-
 # Checks if 2 arguments are passed
 if [ $# -ne 2 ]
 then
@@ -12,14 +11,13 @@ then
     exit 1
 fi
 
-# Check to see if rsync is installed
+# Check if rsync is installed
 if ! command -v rsync > /dev/null 2>&1
 then
     echo "This script requires rsync to be installed"
     echo "Please use your distro's package manager to install rysnc and try again."
     exit 2
 fi
-
 
 # Capture the current date and store it in YYYY-MM-DD format
 current_date=$(date +%Y-%m-%d_%H-%M-%S)
@@ -30,8 +28,7 @@ source=$1
 # Set backup destination
 destination="$2/backups/$1-$current_date"
 
-
-# Checks if backup and log folder exist
+# Check if backup and log folder exist
 if [ ! -d "$2/backups" ];
 then
     mkdir "$2/backups"
@@ -42,20 +39,19 @@ then
     mkdir "$2/backups/logs"
 fi
 
-
-# With dry-run for testing
+# Options with dry-run for testing
 # rsync_options="-avb --backup-dir ./Backups --delete --dry-run"
 
-# Without dry run
+# Options without dry run
 rsync_options="-avb"
 
 # Backup command
 $(which rsync) $rsync_options $source $destination >> $2/backups/logs/$1-$current_date.log
 
-# Keep the 3 latest backups. Gets a list of directories, excluding the three most recent, and deletes them. 
+# Keep the 3 latest backups. Gets a list of directories, excluding the three most recent, and delete them. 
 for backup_dir in $(ls $2/backups/ | sort | head -n -4);
 do
-    # prevent log folder from being removed
+    # Prevent log folder from being removed
     if [ $backup_dir != "logs" ]
     then
         rm -rf "$2/backups/$backup_dir"
