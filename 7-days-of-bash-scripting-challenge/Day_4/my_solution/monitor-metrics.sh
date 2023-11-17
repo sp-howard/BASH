713c27711c26142d0990fd4faf4049ae618f8d32
sp-howard <stevenphoward@gmail.com>
2023-11-16 16:05:10 -0800

    Add Show/Restart Process Feature. Finalize script.

--- 7-days-of-bash-scripting-challenge/Day_4/my_solution/monitor-metrics.sh
+++ 7-days-of-bash-scripting-challenge/Day_4/my_solution/monitor-metrics.sh
@@ -1,112 +1,73 @@
 #!/bin/bash
 
-# This script accepts a single parameter, a process name, and checks if it is running.
-# If not, it will attempt to restart the process a total of 3 times.
-#
-#                              Schedule with Cron
-#
-# With the user you'd like to run the command as, enter the command crontab -e
-# The syntax for a cronjob is as follows, ex:
-#
-# */1 * * * * /path/monitor.sh nginx >> /path/monitor.log 2>&1
-# minute hour day month weekday <command> 
-#
-# The example here the status of the nginx process every minute.
-# 
-# Make sure the user you open the crontab with has access to start processes with systemctl
-#
-# Open /etc/sudoers with a text editor (nano, vim)
-# sudo nano /etc/sudoers
-#
-# Add the following line, changing the placeholder "yourusername"
-# yourusername ALL=NOPASSWD: /bin/systemctl start *, /bin/systemctl stop *
-#
-#                            E-Mail Restart Failures
-# For Ubuntu systems, run:
-# sudo apt install mailutils
-#
+# Initialize metrics
+CPU=0
+MEM=0
+DSK=0
+process_name=""
 
-#!/bin/bash
+# Get CPU usage
+cpu_usage() {
+    CPU=$(top -bn 1 | grep '%Cpu' | awk -v OFS=',' '{sum = $2 + $4 + $6 + $12 + $14; printf("%.2f%", sum)}')
+}
+
+# Get memory usage
+memory_usage() {
+    MEM=$(free | grep 'Mem:' | awk '{used_plus_buffers = $3 + $6; percentage = used_plus_buffers / $2 * 100.0; printf("%.2f%%", percentage)}')
+}
+
+# Get disk space usage
+disk_usage() {
+    DSK=$(df --exclude-type=tmpfs --exclude-type=devtmpfs --output=used,avail | awk 'NR>1 {used+=$1; avail+=$2} END {total=used+avail; printf("%.2f%", used/total*100)}')
+}
 
 show_metrics() {
 
-# CPU Usage
-# Memory
-# Disk Space
+    # Display metrics
+    cpu_usage
+    memory_usage
+    disk_usage
+    
+    # Restore cursor position and clear to the end of screen
+    clear
+    echo "---- System Metrics ----"
+    echo "CPU Usage:  $CPU"
+    echo "Mem Usage:  $MEM"
+    echo "Disk Space: $DSK"
+    
+    # Leave the cursor below the metrics for user input
+    echo
+    echo "Press Enter to Refresh"
+    echo "1. Back to Main Menu"
+    echo "2. Exit"
 
-# top, free, df
-
-# Display Metrics
-cat << EOF
----- System Metrics ----
-
-CPU Usage:  $CPU   Mem Usage:  $MEM   Disk Space:  $DSK
-
-Press Enter to continue...
-EOF
-
-    # Check if username exists in password file
-    # -q sets grep to quiet mode, so that only the exit code is retreived
-    if grep -q "$username" /etc/passwd; then
-        echo "There is already a user named $username."
-        exit 1
-    fi
-
-    useradd $ac_username
-    # Check if useradd command completes
-    if [ $? -ne 0 ]; then
-        echo "Failed to add user. Try running with super user privileges."
-        exit 1
-    else
-        passwd $username
-        echo "New user created: $username"
-    fi
 }
 
 show_process() {
 
+    read -p "Enter process name to monitor: " process_name
+    clear
+
+    if systemctl is-active --quiet $process_name; then
+        echo "$process_name is running."
+    else
+        echo "$process_name is not running."
+    fi
+
+    # Leave the cursor below the metrics for user input
+    echo
+    echo "1. Attempt to Start Process"
+    echo "2. Monitor Different Process"
+    echo "3. Back to Main Menu"
+    echo "4. Exit"
+
 }
 
-finished=0
-
-while [ $finished -ne 1 ]
-do
-    echo "-----   Metrics Monitor   -----"
-
-    echo "1. View System Metrics"
-    echo "2. Monitor a Specific Service"
-    echo "3. Exit"
-
-
-    read input;
-
-    case $input in
-        1) echo "1. View System Metrics";;
-        2) echo "2. Monitor a Specific Service";;
-        3) echo "Exiting script."
-           finished=1;;
-        *) echo "Error: Invalid option. Please choose a valid option (1, 2, or 3)"
-    esac
-done
-
-
-# Check if a single aparameter was passed. If not, prompt user for process name.
-if [ $# -eq 1 ]; then
-    process_name=$1
-else
-    read -p "Enter process name to monitor: " process_name
-fi
-
-# Check if process is running
-if systemctl is-active --quiet $process_name; then
-    echo "$process_name is running."
-    exit 0
-else
-    echo "$process_name is not running."
-
+restart_process() {
     # Attempt to restart the process 3 times
-    for attempt in {1..3};
-    do
+    clear
+    for attempt in {1..3}; do
+
         echo "Attempting to start process, "$process_name". Attempt $attempt of 3"
         sleep 1
         systemctl start $process_name >> /dev/null 2>&1
@@ -114,7 +75,7 @@ else
 
         if systemctl is-active --quiet $process_name; then
             echo "$process_name is running."
-            exit 0
+            break
         else
             echo "$process_name could not be started."
             sleep 1
@@ -126,25 +87,78 @@ else
             echo -ne "\r$countdown"
             sleep 1
         done
-
-        echo ""
-
     done
 
-    echo "$process_name could not be started after 3 attempts. Try starting it manually."
+    # Leave the cursor below the metrics for user input
+    echo
+    echo "1. Attempt to Start Process"
+    echo "2. Monitor Different Process"
+    echo "3. Back to Main Menu"
+    echo "4. Exit"
+}
 
-    # Check if mailutils is installed for email notification
-    if ! command -v mailutils > /dev/null 2>&1
-    then
-        echo "To enable email notification please install mailutils."
-    fi
+main_menu() {
+    while true; do
+        clear
+        echo "-----   Metrics Monitor   -----"
+        echo "1. View System Metrics"
+        echo "2. Monitor a Process"
+        echo "3. Exit"
+        echo
+        
+        read -p "Please select an option: " input
 
-    echo "$process_name could not be started after 3 attempts. Try starting it manually." | mail -s "$process_name could not be started" -a "From: alert@mydomain.com" $notification_recipient
+        case $input in
+            1) 
+                show_metrics
+                while true; do
+                    read input
+                    case $input in
+                        "1")
+                            break
+                            ;;
+                        "2")
+                            exit 0
+                            ;;
+                        *)
+                            show_metrics
+                            ;;
+                    esac
+                done
+                ;;
+            2) 
+                show_process
+                while true; do
+                    read input
+                    case $input in
+                        "1")
+                            restart_process
+                            ;;
+                        "2")
+                            show_process
+                            ;;
+                        "3")
+                            break
+                            ;;
+                        "4")
+                            exit 0
+                            ;;
+                        *)
+                            echo "Error: Invalid option. Please choose a valid option (1, 2, 3, or 4)"
+                            ;;
+                    esac
+                done
+                ;;
+            3) 
+                echo "Exiting script."
+                exit 0
+                ;;
+            *) 
+                echo "Error: Invalid option. Please choose a valid option (1, 2, or 3)"
+                ;;
+        esac
+    done
+}
 
-    exit 1
-fi
- 
- # -p --process
- # -a --alert
- # -f --alert-from
- # -t --alert-to
+# Start the main menu
+main_menu
